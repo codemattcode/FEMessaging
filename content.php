4121e31d71c91aff3bf9537d9034e1c9f7467f01
codemattcode <codemattcode@gmail.com>
2017-03-12 04:34:15 +0000

    add blog styling

--- content.php
+++ content.php
@@ -1,21 +1,29 @@
 <!-- BLOG PAGE INDEX / POSTS LIST TEMPLATE -->
 
-<article>
+<article class="blog-txt">
+<div class="wrapper">
+<div class="row">
 
-<div>
+<div class="col-12">
+<div class="blog-push-down-comp">
 <!-- add post title link / title -->
-      <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      <a style="font-weight: bold;" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 </div>
 <div>
 <!-- add feature image thumbnail -->
       <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
      
 </div>
-<div style="margin-top: 300px;">    
+
+<div>    
 <!-- add post excerpt/content -->
     <?php echo get_the_excerpt(); ?>
       <a href="<?php the_permalink(); ?>">Read More &raquo;</a>
 </div>    
     
+</div><!-- /col-12 -->
 
+</div><!-- /row -->
+</div><!-- /wrapper -->
 </article>
+
